ecda19c162eed0381ba12789717e367390ca0c28
Calvin Joy Tarigan <139420565+cjoyy@users.noreply.github.com>
2024-09-19 06:53:43 +0700

    update REC-1 on myscript.sh

--- TXT/myscript.sh
+++ TXT/myscript.sh
@@ -19,7 +19,7 @@
 REC2="BEEA009D7FE8CCBF"
 # ####################### ####### #### ####
 # REC1: public key
-REC1="BEEA009D7FE8CCBF"
+REC1="63FB12B215403B20"
 # WEEKURL="http://localhost:4000/WEEK/WEEK.txt"
 WEEKURL="https://os.vlsm.org/WEEK/WEEK.txt"
 FILES="my*.asc my*.txt my*.sh"
